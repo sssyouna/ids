e8a28f3845d154f951222a80be4dcaa392ee22cb
sssyouna <younessibnabdeslam@gmail.com>
2026-01-28 16:00:55 -0500

    initial web server configuration

--- http_honeypot/main.py
+++ http_honeypot/main.py
@@ -1,9 +1,34 @@
 import scapy.all as scapy
+from flask import Flask,request,redirect,render_template
+import logging
 
-if __name__ == "__main__":
-    print("Available interfaces:")
-    print(scapy.get_if_list())
+"""
+HTTP Honey Pot
+This script sets up a simple HTTP honeypot using Flask. When accessed, it logs the attacker's IP address and denies access.
+"""
 
-    p = scapy.sniff(count=1)
-    print("Captured packet:")
-    p.show()
+app = Flask(__name__)
+logging.basicConfig(filename='honeypot.log', level=logging.INFO,format='%(asctime)s - %(message)s')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/admin')
+def admin():
+    # Log the intruder's IP address
+    ip_addr = request.remote_addr
+    user_agent = request.headers.get('User-Agent')
+    log_msg = f"Attacker detected: IP={ip_addr}, User-Agent={user_agent}"
+    
+    print(f"[!] {log_msg}") # Print to console
+    logging.info(log_msg) # Log to file
+    
+    return "403 Forbidden: Admin access denied.", 403
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ http_honeypot/scapy-code.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    print("Available interfaces:")
+    print(scapy.get_if_list())
+
+    p = scapy.sniff(count=1)
+    print("Captured packet:")
+    p.show()
